8f96a29201c83e3481201f6b4f7799dea0788b38
Alexandre Perez <alexandrecperez@gmail.com>
2017-07-26 11:14:01 +0100

    Reduce barinel results to a specific node granularity.

--- diagnoser/src/main/java/pt/up/fe/ddsfl/diagnoser/barinel/BarinelRecorder.java
+++ diagnoser/src/main/java/pt/up/fe/ddsfl/diagnoser/barinel/BarinelRecorder.java
@@ -9,10 +9,13 @@ import java.util.Map.Entry;
 
 import pt.up.fe.ddsfl.common.messaging.Message;
 import pt.up.fe.ddsfl.common.messaging.MessageRecorder;
+import pt.up.fe.ddsfl.common.model.Node;
 import pt.up.fe.ddsfl.common.spectrum.Spectrum;
 import pt.up.fe.ddsfl.diagnoser.barinel.Barinel.Result;
 
 public class BarinelRecorder extends MessageRecorder {
+
+    private static final Node.Type typeTarget = Node.Type.METHOD;
 
     public static class BarinelResultMessage implements Message {
 
@@ -42,7 +45,19 @@ public class BarinelRecorder extends MessageRecorder {
         for (Result result : results) {
             System.out.println(result);
             for (Integer probe : result.candidate) {
-                int nodeId = spectrum.getNodeOfProbe(probe).getId();
+                Node node = spectrum.getNodeOfProbe(probe);
+
+                while (node != null) {
+                    if (node.getType() != typeTarget) {
+                        node = node.getParent();
+                    }
+                    else {
+                        break;
+                    }
+                }
+                if (node == null) continue;
+                int nodeId = node.getId();
+
                 if (reducedScores.containsKey(nodeId)) {
                     reducedScores.put(nodeId, reduce(reducedScores.get(nodeId), result.probability));
                 } else {
